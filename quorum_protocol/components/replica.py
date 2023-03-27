15601f0d9b4ee8864e272f6aae251923a2ec2198
rahul20108 <rahul20108@iiitd.ac.in>
2023-03-27 12:21:06 +0530

    quorum inti

---
+++ quorum_protocol/components/replica.py
@@ -0,0 +1,67 @@
+import grpc
+import backup_protocol_pb2 as message
+import backup_protocol_pb2_grpc as servicer
+from port import get_new_port
+import uuid
+from concurrent import futures
+from threading import Lock
+
+class Replica(servicer.ReplicaServicer):
+
+    def __init__(self):
+        super().__init__()
+        self.address='localhost:'+str(get_new_port())
+        self.MAXCLIENTS=10
+        self.uuid=str(uuid.uuid1())
+        # self.CLIENTELE=[]
+        # self.article_list=[]
+        # self.client_lock=Lock()
+        # self.article_lock=Lock()
+        self.registry_channel=grpc.insecure_channel('localhost:5001')
+        pass
+
+    def start(self):
+        try:
+            print('-----STARTING REPLICA------')
+            self.SetupReplica()
+            self.RegisterReplica()
+        except KeyboardInterrupt:
+            print('-----CLOSING REPLICA------')
+            return
+
+    def SetupReplica(self):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=self.MAXCLIENTS))
+        servicer.add_ReplicaServicer_to_server(Replica(),self.server)
+        print("REGISTRY STARTED")
+        self.server.add_insecure_port(self.address)
+
+    def RegisterReplica(self):
+        register_replica_stub = servicer.RegistryServerStub(self.registry_channel)
+        response = register_replica_stub.RegisterReplica(
+            message.ServerMessage(uuid=self.uuid,address=self.address)
+        )
+        print('REPLICA REGISTERED WITH ADDRESS: ',self.address)
+        self.server.start()
+        self.server.wait_for_termination()
+
+    def Read(self, request, context):
+        pass
+
+    def Write(self, request, context):
+        pass
+
+    def Delete(self, request, context):
+        pass
+
+    def HandleWrite(self, request, context):
+        pass
+
+
+def main():
+    my_replica=Replica()
+    my_replica.start()
+    return
+
+
+if __name__=='__main__':
+    main()
