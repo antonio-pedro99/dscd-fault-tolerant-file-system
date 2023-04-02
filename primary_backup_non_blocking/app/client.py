2a13b5212bd1ed85eed7cca3aaa6f785fc39df57
Antonio Pedro <tonio.pedro99@gmail.com>
2023-04-02 18:52:10 +0530

    okk

---
+++ primary_backup_non_blocking/app/client.py
@@ -0,0 +1,85 @@
+import grpc
+import backup_protocol_pb2 as message
+import backup_protocol_pb2_grpc as servicer
+from concurrent import futures
+from google.protobuf import empty_pb2
+import uuid
+from typing import List
+import random
+
+class Client:
+    def __init__(self) -> None:
+        # TODO document why this method is empty
+        self.registry_channel=grpc.insecure_channel('localhost:8888')
+        self.registry_stub = servicer.RegistryServerStub(self.registry_channel)
+        self.replicas = self.__get_replicas__()
+
+
+    def __get_replicas__(self)-> List[message.ServerMessage]:
+        print('----------------\nLIST OF AVAILABLE REPLICAS')
+        try:
+            response = self.registry_stub.GetReplicas(empty_pb2.Empty())
+    
+            return response.serverList
+        except Exception as e:
+            return e
+    
+    def write(self, replica:message.ServerMessage, name=None, content=None , file_uuid=None):
+        
+        replica_stub = servicer.ReplicaStub(channel = grpc.insecure_channel(replica.address) )
+        if file_uuid == None:
+            file_uuid = str(uuid.uuid4())
+        request = message.WriteRequest(name=name, uuid=file_uuid, content=content)
+        response = replica_stub.Write(request)
+        print(response)
+      
+
+
+    def read(self,file_uuid,replica:message.ServerMessage):
+        replica_stub = servicer.ReplicaStub(channel = grpc.insecure_channel(replica.address) )
+        request = message.ReadDeleteRequest(uuid=file_uuid)
+        response = replica_stub.Read(request)
+        print(response)
+
+    def delete(self, file_uuid, replica:message.ServerMessage):
+        replica_stub = servicer.ReplicaStub(channel = grpc.insecure_channel(replica.address) )
+        request = message.ReadDeleteRequest(uuid=file_uuid)
+        response = replica_stub.Delete(request)
+        print(response)
+
+
+def show_menu(client:Client):
+    while True:
+   
+        replica = random.choice(client.replicas)
+
+        print("\n---------MENU--------\n1. Write")
+        print("2. Read\n3. Delete\n4. Exit\n")
+        try:
+            choice=int(input('Choose one option: '))
+            if(choice==1):
+                file_uuid = input("Enter the UUID[Optional]: ")
+                name = input("Enter the file name: ")
+                content = input("Enter the file content: ")
+                if file_uuid != "":
+                    client.write(replica=replica, name = name, content = content, file_uuid = file_uuid)
+                else:
+                    client.write(replica=replica, name = name, content = content)
+            elif(choice==2):
+                file_uuid=input("Enter the uuid of file: ")
+                client.read(file_uuid = file_uuid, replica=replica)
+            elif(choice==3):
+                file_uuid=input("Enter the uuid of file: ")
+                client.delete(file_uuid = file_uuid, replica=replica)
+            elif(choice==4):
+                print("EXITING")
+                return
+        except ValueError:
+            print("[ERROR] Incorrect Input")
+
+def main():
+    my_client=Client()
+    show_menu(my_client)
+
+if __name__=='__main__':
+    main()
---
+++ primary_backup_non_blocking/app/registry.py
@@ -0,0 +1,67 @@
+import grpc
+import backup_protocol_pb2 as message
+import backup_protocol_pb2_grpc as servicer
+from concurrent import futures
+from threading import Lock
+
+class ReplicaRegistryService(servicer.RegistryServerServicer):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.current_registered=0
+        self.replica_list={}
+        self.replica_list_lock=Lock()
+        self.notify_primary_lock = Lock()
+        self.primary_replica=None # we will be saving object of ServerMessage-> it will contain both uuid and address
+        
+    def start(self):
+        try:
+            print("STARTING REGISTRY")
+            registry_replica = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+            servicer.add_RegistryServerServicer_to_server(ReplicaRegistryService(),registry_replica)
+            print("REGISTRY STARTED")
+            registry_replica.add_insecure_port('localhost:8888')
+            registry_replica.start()
+            registry_replica.wait_for_termination()
+        except KeyboardInterrupt:
+            print("------CLOSING REGISTRY------")
+            return
+
+    def RegisterReplica(self, request, context):
+        self.replica_list_lock.acquire()
+        print(f"JOIN REQUEST FROM REPLICA {request.address} [ADDRESS]")        
+        replica=message.ServerMessage(uuid=request.uuid, address=request.address)
+        self.replica_list[request.uuid] = replica
+        # first replica
+        if(len(self.replica_list.keys())==1):
+            self.primary_replica=replica
+            self.replica_list_lock.release()
+            return message.ServerMessage(uuid='EMPTY', address='EMPTY')
+        # not first
+        else:
+            #Notify primary
+            self.notify_primary_lock.acquire()
+            replica_stub = servicer.ReplicaStub(grpc.insecure_channel(self.primary_replica.address))
+            print(self.primary_replica)
+            replica_stub.NotifyPrimary(message.ServerMessage(uuid=replica.uuid, address=replica.address))
+            self.notify_primary_lock.release()
+            self.replica_list_lock.release()
+            return message.ServerMessage(uuid=self.primary_replica.uuid ,
+                                         address=self.primary_replica.address)
+
+    
+    def GetReplicas(self, request, context):
+        self.replica_list_lock.acquire()
+        print("SERVER LIST REQUEST FROM")
+        response =   message.ServerListResponse(serverList=self.replica_list.values())
+        self.replica_list_lock.release()
+        return response
+    
+
+def main():
+    my_registry=ReplicaRegistryService()
+    my_registry.start()
+
+
+if __name__=='__main__':
+    main()
